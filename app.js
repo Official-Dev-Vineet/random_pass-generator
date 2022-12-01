fc17d78efd73555bf62b169eaaa1189d2f3a9591
Official-Dev-Vineet <viratsinghkaharwar8923@gmail.com>
2022-12-01 20:33:05 +0530

    some major changes done

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const inputVal = document.querySelector("#number");
+inputVal.addEventListener('mouseover',()=>{
+    inputVal.focus();
+})
 let pass = [];
 let passText = 'abcdefghijklmnopqrstuvwxyz1234567890{}()|>?<@#$%&*!';
 const passArea = document.querySelector(".password-area");
@@ -8,19 +11,20 @@ const defaultArea = document.querySelector('.default-pass-area');
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     if (inputVal.value > 6 && inputVal.value < 25) {
-        let text = document.createElement('div');
+        let text = document.createElement('textarea');
         text.classList.add('pass');
         var passData = '';
         for (let i = 1; i <= inputVal.value; i++) {
             var random = Math.floor(Math.random() * passText.length)
             passData += passText.substring(random, random + 1)
         }
-        text.innerText = passData + '  =  ' + passData.length;
+        text.textContent = passData
         passArea.appendChild(text);
         pass.push(passData);
+        localStorage.setItem('password',pass);
     }
     else {
-        let text = document.createElement('div');
+        let text = document.createElement('textarea');
         text.classList.add('pass');
         var passData = '';
         for (let i = 1; i <= 12; i++) {
@@ -28,17 +32,25 @@ form.addEventListener("submit", (e) => {
             passData += passText.substring(random, random + 1)
         }
         text.innerText = passData
-        pass=localStorage.getItem('password').split(',')
-        pass=pass.push(passData);
-        localStorage.setItem('password', pass)
-        console.log(pass)
+        defaultArea.appendChild(text);
     }
 });
-var defaultPass=localStorage.getItem('password').split(',');
-
-for (let i = 0; i < defaultPass.length; i++) {
-    let text = document.createElement('div');
-    text.classList.add('pass');
-    text.textContent = defaultPass[i];
-    defaultArea.appendChild(text)
+function getItems(){
+   let pass=localStorage.getItem('password').split(',');
+   pass.map((value,index)=>{
+    let textarea=document.createElement('textarea')
+    textarea.classList.add('pass');
+    textarea.value=value;
+    passArea.appendChild(textarea);
+   })
 }
+getItems();
+clear.addEventListener('click',()=>{
+    localStorage.clear();
+    getItems();
+})
+document.querySelectorAll('textarea').forEach((area)=>{
+    area.addEventListener('mouseover',()=>{
+        area.focus()
+    })
+})
